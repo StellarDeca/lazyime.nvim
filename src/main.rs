edbe1473dd9914188130c29470ca81c9ca298063
StellarDeca <shiwanshuixing@gmail.com>
2025-11-14 18:39:32 +0800

    Implementing IME Windows API in Rust

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
-mod system_ime;
+mod switch;
+mod rpc;
+mod memory;
+mod focus;
 
 fn main() {
-
+    sleep(Duration::from_millis(1000));
+    switch::test();
+    use std::thread::sleep;
+    use std::time::Duration;
+    sleep(Duration::from_millis(1000));
 }
+
